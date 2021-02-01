69669a4fedc7b8d410227fbcd381717c813faa88
jwilson42 <wilson.jd022@gmail.com>
2021-02-01 11:09:51 -0800

    added another example, using an predefined module

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const myModule = require("./aModule");
-const myThirdModule = require("./myOtherModule");
+// Example 1
+// const myModule = require("./aModule");
+// const myThirdModule = require("./myOtherModule");
 
-function main() {
-    console.log("Hello, World");
+// function main() {
+//     console.log("Hello, World");
 
-    const sayHello = (name) => {
-        console.log(`Hello, ${name}`);
-    }
+//     const sayHello = (name) => {
+//         console.log(`Hello, ${name}`);
+//     }
 
-    sayHello("Francis");
-    sayHello("Patrick");
+//     sayHello("Francis");
+//     sayHello("Patrick");
 
-    // console.log(myModule.square(2));
-    // console.log(myModule.power(2, 5));
-    
-    myThirdModule.timeToday();
-}
+//     // console.log(myModule.square(2));
+//     // console.log(myModule.power(2, 5));
 
-main();
+//     myThirdModule.timeToday();
+// }
+
+// main();
+
+// Example 2
+// const fs = require("fs");
+
+
+// fs.readFile("./games.txt", "utf8", (error, data) => {
+//     if (error) {
+//         console.log("An error has occured when reading the file ");
+//     } else {
+//         console.log("Anticpated Games:");
+//         console.log(data);
+//     }
+// });
