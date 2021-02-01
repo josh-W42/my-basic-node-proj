a4fc9be3abc499b3ef0bf722d7e0fe3c9427f924
jwilson42 <wilson.jd022@gmail.com>
2021-02-01 10:20:37 -0800

    node project initialized

---
+++ index.js
@@ -0,0 +1,12 @@
+function main() {
+    console.log("Hello, World");
+
+    const sayHello = (name) => {
+        console.log(`Hello, ${name}`);
+    }
+
+    sayHello("Francis");
+    sayHello("Patrick");
+}
+
+main();
